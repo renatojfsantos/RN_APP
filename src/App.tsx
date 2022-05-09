1a20b880399090d1d2871841e5ec43f8a0aa9061
Renato Santos <renato_jfs@hotmail.com>
2022-05-08 22:05:36 -0300

    add useCallback, useMeno and useRef

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import React, {useState, useEffect} from 'react';
-import {Alert, Button, SafeAreaView, StyleSheet} from 'react-native';
+import React, {useState, useEffect, useCallback, useMemo, useRef} from 'react';
+import {Alert, Button, SafeAreaView, StyleSheet, TextInput} from 'react-native';
 
 import {Header} from './components/Header';
 
@@ -18,38 +18,67 @@ const App = () => {
       setTitle('Olá');
     }
 
-    return () => Alert.alert('Eita!', 'Component unmounting');
+    // return () => Alert.alert('Eita!', 'Component unmounting');
   }, [name, title]);
 
   /**
    * Callback para alterar o nome
    */
-  const handlePressButton = () => {
-    /**
-     * Setando o nome de forma separada utilizando o hook useState
-     */
-    // if (name === 'Bolinha') {
-    //   setName('Renato');
-    // } else {
-    //   setName('Bolinha');
-    // }
+  // const handlePressButton = () => {
+  //   /**
+  //    * Setando o nome de forma separada utilizando o hook useState
+  //    */
+  //   // if (name === 'Bolinha') {
+  //   //   setName('Renato');
+  //   // } else {
+  //   //   setName('Bolinha');
+  //   // }
 
-    /**
-     * Setando passando a função de callback diretamente no state
-     */
-    setName(old => {
-      if (old === 'Bolinha') {
-        return 'Renato';
-      } else {
-        return 'Bolinha';
-      }
-    });
-  };
+  //   /**
+  //    * Setando passando a função de callback diretamente no state
+  //    */
+  //   setName(old => {
+  //     if (old === 'Bolinha') {
+  //       return 'Renato';
+  //     } else {
+  //       return 'Bolinha';
+  //     }
+  //   });
+  // };
+
+  /**
+   * Callback
+   */
+  const handlePressButton = useCallback(() => {
+    if (name === 'Bolinha') {
+      setName('Renato');
+    } else {
+      setName('Bolinha');
+    }
+  }, [name]);
+
+  /**
+   * Memos
+   */
+  const nameChanged = useMemo(() => {
+    // return value
+    return `${name} | ${new Date()}`;
+  }, [name]);
+
+  /**
+   * Refs
+   */
+  const inputRef = useRef<TextInput>({} as TextInput);
 
   return (
     <SafeAreaView style={styles.Container}>
-      <Header title={title} name={name} />
-      <Button title="Change name" onPress={handlePressButton} />
+      <Header title={title} name={nameChanged} />
+      <Button title="Change name" onPress={() => setTitle('title')} />
+      <TextInput
+        style={styles.input}
+        ref={inputRef}
+        onChangeText={text => setName(text)}
+      />
     </SafeAreaView>
   );
 };
@@ -58,6 +87,12 @@ const styles = StyleSheet.create({
   Container: {
     flex: 1,
     margin: 20,
+  },
+  input: {
+    padding: 20,
+    backgroundColor: 'rgba(0,0,0,0.5)',
+    color: 'white',
+    fontSize: 20,
   },
 });
 
