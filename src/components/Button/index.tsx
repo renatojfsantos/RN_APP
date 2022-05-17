978d3139397509210690cc7f4d1a505038d2fe27
Renato Santos <renato_jfs@hotmail.com>
2022-05-17 11:39:45 -0300

    feat: create component button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,15 +1,32 @@
-import React, {useContext} from 'react';
-import {Button as ButtonRN} from 'react-native';
+import React, {useContext, useMemo} from 'react';
 import {ThemeContext} from 'styled-components';
 
-export const Button: React.FC<{onPress: () => void}> = ({onPress}) => {
-  const themeContext = useContext(ThemeContext);
+import {Container, Title, Loading} from './styles';
+import {ButtonProps} from './types';
+
+export const Button = ({
+  children,
+  mode = 'contained',
+  color = 'primary',
+  loading,
+  onPress,
+  ...rest
+}: ButtonProps) => {
+  const {colors} = useContext(ThemeContext);
+
+  const colorByMode = useMemo(() => {
+    return mode === 'outlined' ? colors[color].main : colors[color].onMain;
+  }, [mode, color, colors]);
 
   return (
-    <ButtonRN
-      color={themeContext.colors.primary.main}
-      title="Change theme"
+    <Container
+      mode={mode}
+      borderColor={colors[color].main}
+      color={colors[color].main}
       onPress={onPress}
-    />
+      {...rest}>
+      <Title color={colorByMode}>{children}</Title>
+      {loading && <Loading size={15} color={colorByMode} />}
+    </Container>
   );
 };
---
+++ src/components/Button/styles.ts
@@ -0,0 +1,35 @@
+import styled from 'styled-components/native';
+import {Mode} from './types';
+
+interface ContainerProps {
+  readonly color: string;
+  readonly borderColor: string;
+  readonly mode: Mode;
+}
+
+interface TitleProps {
+  readonly color: string;
+}
+
+export const Container = styled.TouchableOpacity<ContainerProps>`
+  padding: 12px 0;
+  align-items: center;
+  justify-content: center;
+  flex-direction: row;
+  border-width: 1px;
+  border-radius: 10px;
+  border-color: ${({borderColor}) => borderColor || 'transparent'};
+  background-color: ${({color, mode}) =>
+    mode === 'outlined' ? 'transparent' : color};
+`;
+
+export const Title = styled.Text<TitleProps>`
+  color: ${({color}) => color};
+  font-family: ${({theme}) => theme.typography.body2.fontFamily};
+  font-size: ${({theme}) => theme.typography.body2.fontSize}px;
+  align-self: center;
+`;
+
+export const Loading = styled.ActivityIndicator`
+  margin-left: 10px;
+`;
