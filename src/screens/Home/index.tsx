189df15131ac52a557f9b43246bff8ed6c093051
Renato Santos <renato_jfs@hotmail.com>
2022-05-21 16:21:31 -0300

    feat: stack navigation setup

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text} from '~/components/Text';
+
+import {Container} from './styles';
+
+export const Home = () => {
+  return (
+    <Container>
+      <Text>Home</Text>
+    </Container>
+  );
+};
