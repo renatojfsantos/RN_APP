7b61288ef567f1f4225f9536189b6201c6890eac
Renato Santos <renato_jfs@hotmail.com>
2022-05-24 11:53:44 -0300

    feat: navigate nested navigator

--- src/screens/Feed/index.tsx
+++ src/screens/Feed/index.tsx
@@ -7,9 +7,17 @@ export const Feed = ({navigation}) => {
   const handlePress = () => {
     navigation.navigate('Stories');
   };
+
+  const handlePressToPost = () => {
+    navigation.navigate('Profile', {
+      screen: 'Post',
+    });
+  };
+
   return (
     <Container>
       <Button title="Go to Stories" onPress={handlePress} />
+      <Button title="Go to Post" onPress={handlePressToPost} />
     </Container>
   );
 };
